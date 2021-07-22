543efe9dba4c1ab81adcc585b1483e2ca34db0cc
rkopace <greenball@wp.pl>
2021-07-21 22:04:21 -0400

    raf-init

---
+++ sfg-di/src/main/java/guruspringframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,14 @@
+package guruspringframework.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDiApplication.class, args);
+	}
+
+}
+AAA
